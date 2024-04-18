f0f3c6ac1d094b6b10c696a38f49ec91cff26813
Francesco <francesco.daleo@bts.tech>
2024-04-18 11:09:00 +0200

    alfa_done

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('predict-molecule/', views.predict_molecule, name='predict_molecule'),
+    path('predict_molecule/', views.predict_molecule, name='predict_molecule'),
     path('download/',  views.download_view, name='download_view'),
 ]
